3652e93c1723c2ce289c886d1f10702e86ee076f
Hyeonbin Jung <h9jung@uwaterloo.ca>
2025-10-05 10:46:38 -0400

    Add files via upload
    Added Key Financials - income statement in Quarter
    Functional programming for better code organization

--- trading_helper.py
+++ trading_helper.py
@@ -1,43 +1,98 @@
 import xlwings as xw
 import yfinance as yf
 import time
+import pandas as pd
 
 wb = xw.Book(r"C:\Users\sjung\Downloads\trading_helper.xlsx")
 ws = wb.sheets[0]  # Short-term trading sheet
 ws_peer = wb.sheets[2]
 
-#Peer valuation
+# Helper Function
+def add_history(ws, cell, stock, start, end): 
+    ws[cell].value = stock.history(start=start, end=end)["Close"].iloc[0]
+
+def get_quarterly_revenue(ws, cell, stock, ticker, target_quarter):
+    df = stock.quarterly_financials
+    if "Total Revenue" not in df.index:
+        print("No Data found")
+    target = pd.to_datetime(target_quarter)
+    if target in df.columns:
+        ws[cell].value = df.loc["Total Revenue", target]
+    else:
+        print("No Data found")
+        
+def get_quarterly_costofrev(ws, cell, stock, ticker, target_quarter):
+    df = stock.quarterly_financials
+    if "Cost Of Revenue" not in df.index:
+        print("No Data found")
+    target = pd.to_datetime(target_quarter)
+    if target in df.columns:
+        ws[cell].value = df.loc["Cost Of Revenue", target]
+    else:
+        print("No Data found")
+
+def get_quarterly_EBIT(ws, cell, stock, ticker, target_quarter):
+    df = stock.quarterly_financials
+    if "EBIT" not in df.index:
+        print("No Data found")
+    target = pd.to_datetime(target_quarter)
+    if target in df.columns:
+        ws[cell].value = df.loc["EBIT", target]
+    else:
+        print("No Data found")
+
+def get_quarterly_EBITDA(ws, cell, stock, ticker, target_quarter):
+    df = stock.quarterly_financials
+    if "EBITDA" not in df.index:
+        print("No Data found")
+    target = pd.to_datetime(target_quarter)
+    if target in df.columns:
+        ws[cell].value = df.loc["EBITDA", target]
+    else:
+        print("No Data found")
+        
+# Key Financials (Income Statement)
+ticker = ws["E2"].value
+if ticker:
+    stock = yf.Ticker(ticker)
+    get_quarterly_revenue(ws, "E25", stock, ticker, "2024-06-30")
+    get_quarterly_revenue(ws, "F25", stock, ticker, "2024-09-30")
+    get_quarterly_revenue(ws, "G25", stock, ticker, "2024-12-31")
+    get_quarterly_revenue(ws, "H25", stock, ticker, "2025-03-31")
+    get_quarterly_revenue(ws, "I25", stock, ticker, "2025-06-30")
+    get_quarterly_costofrev(ws, "E26", stock, ticker, "2024-06-30")
+    get_quarterly_costofrev(ws, "F26", stock, ticker, "2024-09-30")
+    get_quarterly_costofrev(ws, "G26", stock, ticker, "2024-12-31")
+    get_quarterly_costofrev(ws, "H26", stock, ticker, "2025-03-31")
+    get_quarterly_costofrev(ws, "I26", stock, ticker, "2025-06-30")
+    get_quarterly_EBIT(ws, "E33", stock, ticker, "2024-06-30")
+    get_quarterly_EBIT(ws, "F33", stock, ticker, "2024-09-30")
+    get_quarterly_EBIT(ws, "G33", stock, ticker, "2024-12-31")
+    get_quarterly_EBIT(ws, "H33", stock, ticker, "2025-03-31")
+    get_quarterly_EBIT(ws, "I33", stock, ticker, "2025-06-30")
+    get_quarterly_EBITDA(ws, "E36", stock, ticker, "2024-06-30")
+    get_quarterly_EBITDA(ws, "F36", stock, ticker, "2024-09-30")
+    get_quarterly_EBITDA(ws, "G36", stock, ticker, "2024-12-31")
+    get_quarterly_EBITDA(ws, "H36", stock, ticker, "2025-03-31")
+    get_quarterly_EBITDA(ws, "I36", stock, ticker, "2025-06-30")
+
+# Peer valuation
 tickertarget = ws_peer["F16"].value
 stock_target = yf.Ticker(tickertarget)
 market_cap_target = stock_target.info.get("marketCap")
 ws_peer["J16"].value = market_cap_target 
 
-
+# For Pest 5 years
 ticker = ws["E2"].value
 if ticker:
     stock = yf.Ticker(ticker)
+    add_history(ws, "E11", stock, "2021-12-25", "2022-01-01")
+    add_history(ws, "F11", stock, "2022-12-25", "2023-01-01")
+    add_history(ws, "G11", stock, "2023-12-25", "2024-01-01")
+    add_history(ws, "H11", stock, "2024-12-25", "2025-01-01")
+    add_history(ws, "I11", stock, "2025-06-27", "2025-06-30")
 
-    fy2021 = stock.history(start="2021-12-25", end="2022-01-01")
-    pricefy2021 = fy2021["Close"].iloc[0]
-    ws["E11"].value = pricefy2021
-
-    fy2022 = stock.history(start="2022-12-25", end="2023-01-01")
-    pricefy2022 = fy2022["Close"].iloc[0]
-    ws["F11"].value = pricefy2022
-
-    fy2023 = stock.history(start="2023-12-25", end="2024-01-01")
-    pricefy2023 = fy2023["Close"].iloc[0]
-    ws["G11"].value = pricefy2023
-
-    fy2024 = stock.history(start="2024-12-25", end="2025-01-01")
-    pricefy2024 = fy2024["Close"].iloc[0]
-    ws["H11"].value = pricefy2024
-
-    half_2025 = stock.history(start="2025-06-27", end="2025-06-30")
-    price_half_2025 = half_2025["Close"].iloc[0]
-    ws["I11"].value = price_half_2025
-
-    # Stock price for 7 days
+# Stock price for 7 days
     date7 = ws["E16"].value
     date6 = ws["F16"].value
     date5 = ws["G16"].value
@@ -79,7 +134,10 @@ if ticker:
     if not today0.empty:
         price_today0 = today0["Close"].iloc[0]
         ws["L17"].value = price_today0
-    
+
+
+
+
 while True:
     ticker = ws["E2"].value 
     if ticker:
@@ -88,4 +146,5 @@ while True:
         ws["E7"].value = price  
         ws["F2"].value = time.strftime("%H:%M:%S")  
         print(f"{ticker}: {price}")
-    time.sleep(5)  
+    time.sleep(5)
+
