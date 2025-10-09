703ba561370863794f0bac1fe25822035fb62685
Hyeonbin Jung <h9jung@uwaterloo.ca>
2025-10-08 22:27:10 -0400

    Add files via upload

--- chart_maker.py
+++ chart_maker.py
@@ -7,30 +7,46 @@ import pandas as pd
 
 wb = xw.Book(r"C:\Users\sjung\OneDrive\바탕 화면\새 폴더 (4)\trading_helper.xlsx")
 
-stock = yf.Ticker("TSLA")
-hist = stock.history(start="2025-08-20", end="2025-09-08")
-print(hist.head())
+def days_loader(days, page, cell_Alpha, cell_num):
+    ws = wb.sheets[page]
+    col_num = column_index_from_string(cell_Alpha)
+    title = str(cell_Alpha)+str(cell_num)
+    print (title)
+    ws[title].value = "Date"
+    ws[title].api.Font.Bold = True
+    ws[title].api.Font.Italic = True
+    for i in range(days):
+        reverse = days - i 
+        alpha = get_column_letter(col_num + 1 + i)
+        insert_cell = alpha + str(cell_num)
+        ws[insert_cell].value = (date.today() - timedelta(days=reverse))
 
-def get_chart(days, page, cell_Alpha, cell_num, ticker):
+# Price type: Open(시작가) / High(고점) / Low(저점) / Close(종가) / Volume(거래량)
+# example: get_chart(30, 4, "A", 2, "TSLA", "Close")
+def get_chart(days, page, cell_Alpha, cell_num, ticker, price_type):
     ws = wb.sheets[page]
     col_num = column_index_from_string(cell_Alpha)
     stock = yf.Ticker(ticker)
+    ws[str(cell_Alpha)+str(cell_num)].value = price_type
+    ws[str(cell_Alpha)+str(cell_num)].api.Font.Bold = True
+    ws[str(cell_Alpha)+str(cell_num)].api.Font.Italic = True
     for i in range(days):
         reverse = days - i 
-        alpha = get_column_letter(col_num + i)
-        insert_cell = alpha + str(cell_num)
-        stock_cell = alpha + str(cell_num + 1)
-        ws[insert_cell].value = (date.today() - timedelta(days=reverse))
+        alpha = get_column_letter(col_num + 1 + i)
+        stock_cell = alpha + str(cell_num)
         target_date = (date.today() - timedelta(days=reverse))
         target_date_2 = (date.today() - timedelta(days=reverse - 1))
         try:
-            stock_price = stock.history(start=target_date, end=target_date_2)["Close"].iloc[0]
-            ws[stock_cell].value = stock.history(start=target_date, end=target_date_2)["Close"].iloc[0]
+            stock_price = stock.history(start=target_date, end=target_date_2)[price_type].iloc[0]
+            ws[stock_cell].value = stock.history(start=target_date, end=target_date_2)[price_type].iloc[0]
+            ws[stock_cell].api.Borders.Weight = 2
         except Exception as e:
             ws[stock_cell].value = False
+            ws[stock_cell].api.Borders.Weight = 2
             
-        
-get_chart(30, 4, "A", 1, "TSLA")
+days_loader(30, 4, "A", 1)
+get_chart(30, 4, "A", 2, "TSLA", "Close")
+
 
 
 
